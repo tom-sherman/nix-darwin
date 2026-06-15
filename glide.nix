9508687de33ec464a2caa15b1190c546029ed677
Tom Sherman <tom@sherman.is>
2026-06-15 11:23:48 +0100

    Update glide

--- glide.nix
+++ glide.nix
@@ -6,11 +6,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "glide";
-  version = "0.2.12";
+  version = "0.2.13";
 
   src = fetchurl {
     url = "https://github.com/glide-wm/glide/releases/download/v${version}/Glide_${version}_aarch64.dmg";
-    sha256 = "sha256-x6V79ntvA7RKT7KLPW8IZn4i3hKkatP4WtCL6MIUN3s=";
+    sha256 = "sha256-87rwvaOO3SQMNAABAxgbzHulYUE96ruxDIBOxsu7T1Y=";
   };
 
   nativeBuildInputs = [ undmg ];
